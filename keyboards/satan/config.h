de6ed8053053e5c945b1920f4d81b7feb54a1bee
Alex Chin <alex.ba.chin@gmail.com>
2017-09-29 22:27:49 -0400

    Remove tapping toggle from function layer.

--- keyboards/satan/config.h
+++ keyboards/satan/config.h
@@ -49,8 +49,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
 
-#define TAPPING_TOGGLE 2
-
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
